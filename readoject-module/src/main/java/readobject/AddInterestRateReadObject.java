de4638b25091a7d4e5616e34c2fec34f030de52b
Coravu Jean <jcoravu@yahoo.com>
2025-06-19 15:00:07 +0300

    Refactor code

--- readoject-module/src/main/java/readobject/AddInterestRateReadObject.java
+++ readoject-module/src/main/java/readobject/AddInterestRateReadObject.java
@@ -1,31 +1,23 @@
 package readobject;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Setter
+@NoArgsConstructor
 public class AddInterestRateReadObject {
 
     // Attribute for interest rate
     private double interestRate;
-
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate activationDate;
 
     //Default constructor
-    public AddInterestRateReadObject() {
-    }
-
-    public double getInterestRate() {
-        return interestRate;
-    }
-
-    public void setInterestRate(double interestRate) {
-        this.interestRate = interestRate;
-    }
-
-    public LocalDate getActivationDate() {
-        return activationDate;
-    }
-
-    public void setActivationDate(LocalDate activationDate) {
-        this.activationDate = activationDate;
-    }
 }
