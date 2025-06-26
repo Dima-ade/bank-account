eecc817446c37a0059020f313a2e0997e3a634a2
Coravu Jean <jcoravu@yahoo.com>
2025-06-26 12:36:09 +0300

    Refactor code

--- db-module/src/main/java/ro/adela/bank/service/PersistenceManager.java
+++ db-module/src/main/java/ro/adela/bank/service/PersistenceManager.java
@@ -3,7 +3,6 @@ package ro.adela.bank.service;
 import lombok.experimental.FieldNameConstants;
 import ro.adela.bank.jpa.repository.JpaAmountHistoryRepository;
 import ro.adela.bank.jpa.repository.JpaBankAccountRepository;
-import ro.adela.bank.jpa.repository.JpaBankDataRepository;
 import ro.adela.bank.jpa.repository.JpaInterestRateRepository;
 
 public class PersistenceManager {
@@ -12,14 +11,11 @@ public class PersistenceManager {
 
     private final JpaBankAccountRepository jpaBankAccountRepository;
 
-    private JpaBankDataRepository jpaBankDataRepository;
-
     private JpaInterestRateRepository jpaInterestRateRepository;
 
-    public PersistenceManager(JpaAmountHistoryRepository jpaAmountHistoryRepository, JpaBankAccountRepository jpaBankAccountRepository, JpaBankDataRepository jpaBankDataRepository, JpaInterestRateRepository jpaInterestRateRepository) {
+    public PersistenceManager(JpaAmountHistoryRepository jpaAmountHistoryRepository, JpaBankAccountRepository jpaBankAccountRepository, JpaInterestRateRepository jpaInterestRateRepository) {
         this.jpaAmountHistoryRepository = jpaAmountHistoryRepository;
         this.jpaBankAccountRepository = jpaBankAccountRepository;
-        this.jpaBankDataRepository = jpaBankDataRepository;
         this.jpaInterestRateRepository = jpaInterestRateRepository;
     }
 
@@ -31,10 +27,6 @@ public class PersistenceManager {
 
     public JpaBankAccountRepository getJpaBankAccountRepository() {
         return jpaBankAccountRepository;
-    }
-
-    public JpaBankDataRepository getJpaBankDataRepository() {
-        return jpaBankDataRepository;
     }
 
     public JpaInterestRateRepository getJpaInterestRateRepository() {
--- dto-interfaces-module/src/main/java/ro/adela/bank/BankDataDto.java
+++ dto-interfaces-module/src/main/java/ro/adela/bank/BankDataDto.java
@@ -19,29 +19,14 @@ import java.util.List;
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class BankDataDto {
-
-    //@Id
-    //@JsonIgnore
-    //@XmlTransient
-    private Integer id;
-//    @OneToMany(cascade = CascadeType.ALL)
-//    //define the column in the bank account table (the child) which will be
-//    // foreign key to the parent table entity (bank data table)
-//    @JoinColumn(name = "bank_account_id", nullable = false, referencedColumnName = "id")
     @XmlElement(name = "account")
     @JsonProperty("accounts")
     private List<BankAccountDto> accounts;
-//    @OneToMany(cascade = CascadeType.ALL)
-//    //define the column in the amount history table (the child) which will be
-//    // foreign key to the parent table entity (bank data table)
-//    @JoinColumn(name = "amount_history_id", nullable = false, referencedColumnName = "id")
+
     @XmlElement(name = "amount")
     @JsonProperty("amounts")
     private List<AmountHistoryDto> amounts;
-//    @OneToMany(cascade = CascadeType.ALL)
-//    //define the column in the interest rate table (the child) which will be
-//    // foreign key to the parent table entity (bank data table)
-//    @JoinColumn(name = "interest_rate_id", nullable = false, referencedColumnName = "id")
+
     @XmlElement(name = "interest")
     @JsonProperty("interests")
     private List<InterestRateDto> interests;
